faae89dca1f5415e3ce5adc8ff6fec26e33d9763
Jrockbritto <jrockbritto@gmail.com>
2023-04-28 23:42:31 -0300

    chore: decided to leave the localhost with the correct dates for now

--- src/utils/timezone.function.ts
+++ src/utils/timezone.function.ts
@@ -1,6 +1,6 @@
 import { addMinutes } from "date-fns";
 
 export const removeTimezoneOffset = (date: Date): Date => {
-  const TIME_ZONE_OFFSET = 300;
-  return addMinutes(date, TIME_ZONE_OFFSET);
+  const timeOffset = date.getTimezoneOffset();
+  return addMinutes(date, timeOffset);
 };
