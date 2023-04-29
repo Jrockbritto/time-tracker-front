3b1e912c1e63137cae9616f8ab0c813cb7c5c464
Jrockbritto <jrockbritto@gmail.com>
2023-04-28 23:04:38 -0300

    fix: fixing time zone offset

--- src/utils/timezone.function.ts
+++ src/utils/timezone.function.ts
@@ -1,6 +1,6 @@
 import { addMinutes } from "date-fns";
 
 export const removeTimezoneOffset = (date: Date): Date => {
-  const offSet = date.getTimezoneOffset();
-  return addMinutes(date, offSet);
+  const TIME_ZONE_OFFSET = 300;
+  return addMinutes(date, TIME_ZONE_OFFSET);
 };
